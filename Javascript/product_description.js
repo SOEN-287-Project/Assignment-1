df0e12b8aac65ed16d7b3e3c5f653047873c6a19
unknown <alexandar.petrov@yahoo.com>
2022-03-24 23:44:23 -0400

    I added functionality to the description button on page 3 (product description page).

--- Javascript/product_description.js
+++ Javascript/product_description.js
@@ -51,39 +51,64 @@ window.addEventListener('load', async() => {
 
 
 
-function update_input(){
+function update_input() {
     let quantity = document.getElementById("quantity_input").value;
     // let quantity = localStorage.getItem('foodCounter');
     foodCounter = document.getElementById("quantity_input").value;
-    localStorage.setItem(`${url1.get('product')}foodCounter`,foodCounter);
+    localStorage.setItem(`${url1.get('product')}foodCounter`, foodCounter);
 
     console.log(localStorage.getItem(`${url1.get('product')}foodCounter`));
 
-    if(foodCounter<0){ //Alert if below 0 (invalid)
+    if (foodCounter < 0) { //Alert if below 0 (invalid)
         document.getElementById("price_update").style.color = "red";
 
-    }else{
+    } else {
         document.getElementById("price_update").style.color = "#32CD32";
 
     }
 
-    if(window.location.href.indexOf("?") == -1){
-        history.replaceState("","",window.location.href+"?quantity="+foodCounter) //Updates quantity parameter in url
+    if (window.location.href.indexOf("?") == -1) {
+        history.replaceState("", "", window.location.href + "?quantity=" + foodCounter) //Updates quantity parameter in url
 
-    }else{
+    } else {
 
-        if(window.location.href.indexOf("&quantity")!=-1){
-            history.replaceState("","",window.location.href.substring(0,window.location.href.indexOf("&quantity"))+"&quantity="+foodCounter) //Updates quantity parameter in url
+        if (window.location.href.indexOf("&quantity") != -1) {
+            history.replaceState("", "", window.location.href.substring(0, window.location.href.indexOf("&quantity")) + "&quantity=" + foodCounter) //Updates quantity parameter in url
 
-        }else{
-            history.replaceState("","",window.location.href +"&quantity="+foodCounter) //Updates quantity parameter in url
+        } else {
+            history.replaceState("", "", window.location.href + "&quantity=" + foodCounter) //Updates quantity parameter in url
 
         }
 
     }
 
-    document.getElementById("price_update").innerHTML = "$"+foodCounter*price; //Update price
+    document.getElementById("price_update").innerHTML = "$" + foodCounter * price; //Update price
 
 
+}
+
+function check_block(){
+
+    if(sessionStorage.getItem("bool") == "block"){
+
+        document.getElementById("description").style.display = "block";
+
+    }
+
+}
+
+function description_button(){
+
+    if(document.getElementById("description").style.display == "none"){
+
+        document.getElementById("description").style.display = "block";
+        sessionStorage.setItem("bool", "block");
+    }
+
+    else{
+
+        document.getElementById("description").style.display = "none";
+        sessionStorage.setItem("bool", "none");
+    }
 
 }
